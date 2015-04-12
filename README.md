9e5073cd85f128553f6475a190032a127332493f
iamsujun <iamsujun@gmail.com>
2015-04-12 18:58:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Windows下WNMP启动相关脚本
+    windows+nginx+memcache+php+redis
+
+## 安装步骤
+1. 下载windows-lnmp到电脑上；
+2. 在D盘创建server文件夹；
+3. 将windows-lnmp中的文件拷贝至D:\server中；
+4. 在server文件夹下安装nginx、php、redis、memcache；
+5. 分别配置nginx、php；
+
+## 文件列表
+- README.md 说明文件
+- startService.bat 启动服务
+- stopService.bat 停止服务
+- restartService.bat 重启服务
+- RunHiddenConsole.exe  Windows下隐藏DOS命令行窗口的程序
+
+![](./image/list-pic.png)
---
+++ startService.bat
@@ -0,0 +1,22 @@
+@echo off
+
+rem "set path"
+SET base_path=d:\Server
+SET config_path=%base_path%\config
+
+SET nginx_path=%base_path%\nginx
+SET php_path=%base_path%\php
+SET redis_path=%base_path%\redis
+SET memcache_path=%base_path%\memcached
+
+echo Starting Redis...
+RunHiddenConsole.exe %redis_path%\redis-server.exe
+
+echo Starting memcache...
+RunHiddenConsole.exe %memcache_path%\memcached.exe
+
+echo Starting PHP FastCGI...
+RunHiddenConsole.exe %php_path%\php-cgi.exe -b 127.0.0.1:9000 -c %base_path%\php.ini
+
+echo starting Nginx...
+RunHiddenConsole.exe %nginx_path%\nginx.exe -p %nginx_path%
